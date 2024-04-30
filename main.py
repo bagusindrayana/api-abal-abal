891560a0aa1af2b5e44a3287ec1b274bc55b2fd4
bagusindrayana <bagusindrayanaindo@gmail.com>
2024-04-30 14:14:27 +0800

    hide code

--- main.py
+++ main.py
@@ -39,11 +39,11 @@ async def message(
     )
 
 
-default_port = "8080"
-try:
-    port = int(float(os.environ.get("PORT", default_port)))
-except TypeError:
-    port = int(default_port)
+# default_port = "8080"
+# try:
+#     port = int(float(os.environ.get("PORT", default_port)))
+# except TypeError:
+#     port = int(default_port)
 
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
+# if __name__ == "__main__":
+#     uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
