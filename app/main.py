2f1b6978379208bc0263b3daa383434bc85c2446
bagusindrayana <bagusindrayanaindo@gmail.com>
2024-04-30 14:21:55 +0800

    update response hellow world

--- app/main.py
+++ app/main.py
@@ -26,7 +26,7 @@ class RequestMessage(BaseModel):
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Hello World Abal-Abal"}
 
 
 @app.post("/request-message/")
